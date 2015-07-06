4c4f81607cda014fe38ce439433d3c026f7921c6
Khurram Virani <kvirani@gmail.com>
2015-07-06 09:58:43 -0400

    v simple almost non existent OOP implementation, almost complete

---
+++ app.rb
@@ -0,0 +1,7 @@
+require './game'
+
+puts "hello"
+
+game = Game.new
+game.play
+# game.display_rules # wont work
--- game.rb
+++ game.rb
@@ -1,5 +1,51 @@
 class Game
 
+
+  def initialize
+    # ???
+    puts "in init"
+  end
+
+  def play
+    puts "WEEEEEE!!!"
+    display_rules
+
+    points = 0
+
+    print "Your Name: "
+    player_name = gets.chomp
+
+    puts "Welcome: #{player_name}"
+
+    while true
+      number = 7 # TODO: randomize
+
+      print "Guess number: "
+      guess = gets.chomp.to_i
+
+      if guess == number
+        puts "WOW SMACK ON"
+        points += 5
+      elsif guess - 1 == number || guess + 1 == number
+        puts "VERY CLOSE!"
+        points += 3
+      else
+        puts "NOPE"
+        points -= 1
+      end
+
+
+    end
+
+  end
+
+  private 
+
+  def display_rules
+    puts "Rules of the game follow:"
+    puts "...."
+  end
+
   
 
 end
